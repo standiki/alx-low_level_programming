dd7d4b8bf572230cba4325ea729d8c3c1738b4bb
stanflows <atakistanley@gmail.com>
2021-06-17 15:14:12 -0700

    prints varoius data & integer types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
+#include <limits.h>
 
 /**
- *main - prints the size of various data types | integer types
+ * main - Prints various data types and integer types
  *
- *Return: 0 on success
+ * Return: 0 on success
  */
 
 int main(void)
 {
-	prinf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
-	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
