64b869e8226b333b297a579e2359882bc9431a3c
stanflows <atakistanley@gmail.com>
2021-06-17 12:26:12 -0700

    4-puts.c file added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the message "Programming is like building a multilingual puzzle," using put function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
+}
