758e2ebd79bb529e944276712e39197a537e05f4
stanflows <atakistanley@gmail.com>
2021-06-17 14:27:21 -0700

    4-puts.c updated

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,11 @@
 /**
  *main - prints "Programming is like building a multilingual puzzle"
  *
- *Return: 0 if successful
+ *Return: 0 on sucess
  */
 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	
-	return(0);
+	return (0);
 }
