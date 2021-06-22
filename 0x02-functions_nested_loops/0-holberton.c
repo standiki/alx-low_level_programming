2476f920fa298681da50673bfe7b774ce0ec01f4
stanflows <atakistanley@gmail.com>
2021-06-22 13:16:43 -0700

    Printing the word  Holberton with a new line

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *main - Prints the word "Holberton" followed by a new line
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+	char str;
+	for (str = Holberton"; str <= 9; str++)
+	{
+		_putchar(str);
+	}
+
+	_putchar('\n');
+	return (0);
+}
