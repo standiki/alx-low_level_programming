d7da64dad6219a323cca2a5760a0be4cdd1e0a45
stanflows <atakistanley@gmail.com>
2021-06-23 13:09:53 -0700

    Fn checks for lowercase char

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_islower - checks lower character
+ *c: to be checked
+ *
+ *Return: 1 or 0
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
