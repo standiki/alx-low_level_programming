e07829e274d9fc79acedab51e7748b76a7e17c7d
stanflows <atakistanley@gmail.com>
2021-06-24 13:03:52 -0700

    Prints num from 0 - 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - print numbers from 0 to 9
+ *Return: void
+ */
+void print_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar(num + '0');
+	}
+	_putchar('\n');
+}
