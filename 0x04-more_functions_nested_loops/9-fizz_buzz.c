3e14b6e6e1c89aa72e455fdc050d3c17ad7c1c0a
stanflows <atakistanley@gmail.com>
2021-06-24 13:44:03 -0700

    Fn prints 1 - 100 ex 3 and 5

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ *main - prints from 1 - 100 excluding 3 and 5
+ *Return: 0 
+ */
+int main(void)
+{
+	int i, t, f;
+	int a;
+	int b;
+
+	for (i = 1; i <= 100; i++)
+	{
+		a = i % 3;
+		b = i % 5;
+
+		if (a == 0 && b == 0)
+			printf("FizzBuzz");
+		else if (b == 0)
+			printf("Buzz");
+		else if (a == 0)
+			printf("Fizz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			putchar(' ');
+	}
+	printf("\n");
+	return (0);
+}
