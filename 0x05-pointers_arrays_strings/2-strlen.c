180af1e8df97ea60764b2dd763c3b55613601d1b
stanflows <atakistanley@gmail.com>
2021-06-29 12:46:05 -0700

    Fn returns length of strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ *_strlen - returns the lenght of a string
+ *
+ *@str:  string
+ *Return: str length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	
+	while (*s++)
+		lenght++;
+
+	return (lenght);
+}
