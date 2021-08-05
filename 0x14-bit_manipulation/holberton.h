06228973e835d1f573aa86a9998ce5859179fc7d
stanflows <atakistanley@gmail.com>
2021-08-05 10:24:35 -0700

    Added header file

--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -1,6 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+/* Functions prototypes */
+
 unsigned int binary_to_uint(const char *b);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 void print_binary(unsigned long int n);
