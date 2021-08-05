6da6c3de0c4b5149e304cd0df7aa21743f8ecd66
stanflows <atakistanley@gmail.com>
2021-08-05 10:40:16 -0700

    Header file for functions added

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+/* Functions prototypes */
+
+unsigned int binary_to_uint(const char *b);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+void print_binary(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+int _putchar(char c);
+
+#endif /* ifndef HOLBERTON_H */
